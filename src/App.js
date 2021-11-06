f4a20c898c81385d9c87c27c89e83715d7e9806c
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-11-06 11:05:10 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+let todoList = ['item1', 'item2', 'item3'];
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {todoList.map((element) => {
+          return <li>test</li>;
+        })}
+      </ul>
     </div>
   );
 }
