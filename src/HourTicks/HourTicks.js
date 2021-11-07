063d0a9577f964d2e2d234f5d157d6afd98882ee
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-11-07 20:13:47 -0300

    Added ticks and basic hours, not styled

--- src/HourTicks/HourTicks.js
+++ src/HourTicks/HourTicks.js
@@ -1,9 +1,25 @@
 import styled from 'styled-components';
 
 export default function HourTicks() {
+  let startingTime = 9;
+  let endingTime = 18;
+  let hours = [];
+  for (let i = startingTime; i <= endingTime; i++) {
+    hours.push(i);
+  }
+
   return (
     <>
-      <MainWrapper></MainWrapper>
+      <MainWrapper>
+        {hours.map((element, index) => {
+          return (
+            <HourBlockElement key={index}>
+              <HourTicker>{element}</HourTicker>
+              <HourLine></HourLine>
+            </HourBlockElement>
+          );
+        })}
+      </MainWrapper>
     </>
   );
 }
@@ -11,8 +27,19 @@ export default function HourTicks() {
 const MainWrapper = styled.div`
   width: 100%;
   height: 100%;
-  background: #51cf66;
   position: relative;
   z-index: 0;
   left: -100px;
+`;
+
+const HourBlockElement = styled.div``;
+
+const HourTicker = styled.p`
+  padding: 30px;
+`;
+
+const HourLine = styled.div`
+  border-bottom: 1px solid red;
+  height: 1px;
+  width: 100%;
 `;
