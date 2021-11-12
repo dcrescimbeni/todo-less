bb433afadbdf9adae85877f0ee7e2bac51437976
dcrescimbeni <dc@estudiocrescimbeni.com.ar>
2021-11-12 20:47:17 -0300

    styled task list

--- src/TaskColumn/TaskColumn.js
+++ src/TaskColumn/TaskColumn.js
@@ -55,8 +55,10 @@ export default function TaskColumn({
                               handleToggledTask(element.id, e.target.checked)
                             }
                           />
-                          {element.description}, duration: {element.duration},
-                          color: {element.color}
+                          <TaskDescription>
+                            {element.description}
+                          </TaskDescription>
+                          <TaskDuration>{element.duration} hour</TaskDuration>
                         </TaskElement>
                       )}
                     </Draggable>
@@ -75,20 +77,38 @@ export default function TaskColumn({
 const ListWrapper = styled.ul`
   list-style: none;
   padding: 0px;
+  margin: 0px 40px;
+  /* border: 1px solid red; */
 `;
 
 const TaskElement = styled.li`
   margin: 0px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px solid #dee2e6;
 `;
 
 const CheckBox = styled.input`
   appearance: none;
-  margin: 3px;
-  width: 1.15rem;
-  height: 1.15rem;
   border: 0.15rem solid #${(props) => props.color};
   border-radius: 50%;
   transform: translateY(0.3rem);
+  height: 20px;
+  flex-grow: 0;
+  flex-shrink: 0;
+  flex-basis: 20px;
+  margin-top: -4px;
 `;
 
-// TODO: Add drag and drop
+const TaskDescription = styled.p`
+  flex-grow: 1;
+  margin-left: 20px;
+  margin-right: 20px;
+`;
+
+const TaskDuration = styled.p`
+  flex-shrink: 0;
+  font-size: 12px;
+  color: #adb5bd;
+`;
