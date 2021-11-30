187cc103d44d0f1019b8b95dd04abe80893feef7
Dino <crescimbenidino@gmail.com>
2021-11-29 21:31:02 -0300

    Edit prompt changes

--- src/EditPrompt/EditPrompt.js
+++ src/EditPrompt/EditPrompt.js
@@ -17,48 +17,68 @@ export default function EditPrompt({
     toggleEdit();
   }
 
-  console.log(editVisible);
-
   return (
     <Dialog isOpen={editVisible} onDismiss={toggleEdit} aria-label="Edit task">
-      <InformationWrapper>
-        <DescriptionInput
-          key={element.id}
-          type="text"
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        ></DescriptionInput>
-        <HourInput
-          type="text"
-          value={newDuration}
-          onChange={(e) => setNewDuration(e.target.value)}
-        ></HourInput>
-        <HourIndicator> hours</HourIndicator>
-      </InformationWrapper>
-      <ButtonWrapper>
-        <SaveButton
-          type="button"
-          value="Save"
-          onClick={handleSaveEdit}
-        ></SaveButton>
-        <DeleteButton
-          type="button"
-          value="Delete"
-          onClick={(e) => {
-            handleDelete(element.id);
-          }}
-        ></DeleteButton>
-      </ButtonWrapper>
+      <button className="close-button" onClick={toggleEdit}>
+        <span aria-hidden>x</span>
+      </button>
+      <MainWrapper>
+        <DescriptionHelper>Edit task</DescriptionHelper>
+        <InformationWrapper>
+          <DescriptionInput
+            key={element.id}
+            type="text"
+            value={newDescription}
+            onChange={(e) => setNewDescription(e.target.value)}
+          ></DescriptionInput>
+          <HourWrapper>
+            <HourInput
+              type="text"
+              value={newDuration}
+              onChange={(e) => setNewDuration(e.target.value)}
+            ></HourInput>
+            <HourIndicator> hours</HourIndicator>
+          </HourWrapper>
+        </InformationWrapper>
+        <ButtonWrapper>
+          <SaveButton
+            type="button"
+            value="Save"
+            onClick={handleSaveEdit}
+          ></SaveButton>
+          <CancelButton
+            type="button"
+            value="Cancel"
+            onClick={toggleEdit}
+          ></CancelButton>
+          <DeleteButton
+            type="button"
+            value="Delete"
+            onClick={(e) => {
+              handleDelete(element.id);
+            }}
+          ></DeleteButton>
+        </ButtonWrapper>
+      </MainWrapper>
     </Dialog>
   );
 }
 
-const Wrapper = styled.div`
+const MainWrapper = styled.div`
   display: flex;
-  width: 100%;
-  padding: 15px 10px;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  margin: 20px;
+  padding: 15px;
+  border: 2px solid #dee2e6;
+  border-radius: 5px;
+`;
+
+const DescriptionHelper = styled.p`
+  font-size: 12px;
+  color: #adb5bd;
+  display: block;
+  margin: 5px;
 `;
 
 const InformationWrapper = styled.div`
@@ -74,16 +94,20 @@ const ButtonWrapper = styled.div`
 `;
 
 const DescriptionInput = styled.input`
-  margin-left: auto;
-  flex-grow: 1;
-  padding: 5px;
+  font-size: 20px;
+  font-weight: bold;
+  color: #212529;
+  padding: 15px 5px;
+  border: 0px;
 `;
 
 const HourInput = styled.input`
+  font-weight: bold;
+  font-size: 16px;
   width: 2rem;
-  margin: 0px 5px;
-  padding: 5px;
+  border: 0px;
   text-align: right;
+  margin-right: 5px;
 `;
 
 const HourIndicator = styled.div`
@@ -104,6 +128,18 @@ const SaveButton = styled.input`
   flex-grow: 9;
 `;
 
+const CancelButton = styled.input`
+  background: grey;
+  color: white;
+  font-weight: bold;
+  color: white;
+  padding: 10px;
+  margin-right: 5px;
+  border: 0px;
+  border-radius: 5px;
+  flex-grow: 1;
+`;
+
 const DeleteButton = styled.input`
   background: #ef4444;
   color: white;
@@ -113,4 +149,9 @@ const DeleteButton = styled.input`
   border: 0px;
   border-radius: 5px;
   flex-grow: 1;
+`;
+
+const HourWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
