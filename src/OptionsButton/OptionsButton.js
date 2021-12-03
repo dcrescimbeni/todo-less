6c1c0fa9f7976777148c39877e72027eb990ea93
Dino <crescimbenidino@gmail.com>
2021-12-02 21:03:59 -0300

    Replaced options text with edit icon

--- src/OptionsButton/OptionsButton.js
+++ src/OptionsButton/OptionsButton.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { Menu, MenuList, MenuButton, MenuItem } from '@reach/menu-button';
 import '@reach/menu-button/styles.css';
+import { MdEdit } from 'react-icons/md';
 
 import './OptionsButton.css';
 
@@ -8,7 +9,9 @@ export default function OptionsButton({ element, handleDelete, toggleEdit }) {
   return (
     <Wrapper>
       <Menu>
-        <MenuButton>Options</MenuButton>
+        <MenuButton>
+          <MdEdit />
+        </MenuButton>
         <MenuList>
           <MenuItem onSelect={toggleEdit}>Edit</MenuItem>
           <MenuItem onSelect={(e) => handleDelete(element.id)}>Delete</MenuItem>
