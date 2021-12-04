a45d30ffff78dd259db8d6a36ff6c9e14585c25c
Dino <crescimbenidino@gmail.com>
2021-12-04 12:30:51 -0300

    Styled dialog

--- src/AddItem/AddItem.js
+++ src/AddItem/AddItem.js
@@ -63,100 +63,142 @@ export default function AddItem({ handleAdd, showDialog, setShowDialog }) {
       aria-label="Add task"
       initialFocusRef={inputRef}
     >
-      <button className="close-button" onClick={close}>
-        <span aria-hidden>x</span>
-      </button>
+      <CloseButtonWrapper>
+        <button className="close-button" onClick={close}>
+          <span aria-hidden>+</span>
+        </button>
+      </CloseButtonWrapper>
       <MainWrapper>
-        <DescriptionHelper>Add task</DescriptionHelper>
-        <DescriptionInput
-          type="text"
-          value={taskDescription}
-          name="itemDescription"
-          id="itemDescription"
-          placeholder="Description"
-          onChange={(e) => setTaskDescription(e.target.value)}
-          autoComplete="off"
-          ref={inputRef}
-        />
-        <PickerWrapper>
-          <DurationWrapper>
-            <DurationInput
-              type="number"
-              min="1"
-              value={taskDuration}
-              name="itemDuration"
-              id="itemDuration"
-              onChange={(e) => setTaskDuration(parseInt(e.target.value))}
-              autoComplete="off"
-            />
-            {taskDuration > 1 ? 'hours' : 'hour'}
-          </DurationWrapper>
+        <TaskInformation>
+          <DescriptionHelper>Add task</DescriptionHelper>
+          <DescriptionInput
+            type="text"
+            value={taskDescription}
+            name="itemDescription"
+            id="itemDescription"
+            placeholder="Description"
+            onChange={(e) => setTaskDescription(e.target.value)}
+            autoComplete="off"
+            ref={inputRef}
+          />
+          <PickerWrapper>
+            <DurationWrapper>
+              <DurationInput
+                type="number"
+                min="1"
+                value={taskDuration}
+                name="itemDuration"
+                id="itemDuration"
+                onChange={(e) => setTaskDuration(parseInt(e.target.value))}
+                autoComplete="off"
+              />
+              {taskDuration > 1 ? 'hours' : 'hour'}
+            </DurationWrapper>
 
-          <ColorWrapper>
-            <ColorOrb color={taskColor}></ColorOrb>
-            <Listbox
-              onChange={(e) => {
-                setTaskColor(e);
-              }}
-            >
-              <ListboxOption value="3B82F6">Blue</ListboxOption>
-              <ListboxOption value="EF4444">Red</ListboxOption>
-              <ListboxOption value="10B981">Green</ListboxOption>
-              <ListboxOption value="F59E0B">Yellow</ListboxOption>
-            </Listbox>
-          </ColorWrapper>
-        </PickerWrapper>
-        <AddTaskButton type="button" value="Add" onClick={saveAndExit} />
-        <CancelButton
-          type="button"
-          value="Cancel"
-          onClick={close}
-        ></CancelButton>
-        {errorArray.map((element) => {
-          return element;
-        })}
+            <ColorWrapper>
+              <ColorOrb color={taskColor}></ColorOrb>
+              <Listbox
+                onChange={(e) => {
+                  setTaskColor(e);
+                }}
+              >
+                <ListboxOption value="3B82F6">Blue</ListboxOption>
+                <ListboxOption value="EF4444">Red</ListboxOption>
+                <ListboxOption value="10B981">Green</ListboxOption>
+                <ListboxOption value="F59E0B">Yellow</ListboxOption>
+              </Listbox>
+            </ColorWrapper>
+          </PickerWrapper>
+        </TaskInformation>
+        <ButtonsWrapper>
+          <AddTaskButton type="button" value="Add" onClick={saveAndExit} />
+          <CancelButton
+            type="button"
+            value="Cancel"
+            onClick={close}
+          ></CancelButton>
+          {errorArray.map((element) => {
+            return element;
+          })}
+        </ButtonsWrapper>
       </MainWrapper>
     </Dialog>
   );
 }
 
+// Main
+
 const MainWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  margin: 20px;
-  padding: 15px;
-  border: 2px solid #dee2e6;
-  border-radius: 5px;
+  padding: 0px 20px;
 `;
+
+const CloseButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: right;
+`;
+
+const PickerWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 0px;
+`;
+
+// Task
+
+const TaskInformation = styled.div`
+  border-bottom: 1px solid #adb5bd;
+`;
+
+// Task > Description
 
 const DescriptionHelper = styled.p`
   font-size: 12px;
   color: #adb5bd;
   display: block;
-  margin: 5px;
+  margin-bottom: 10px;
 `;
 
 const DescriptionInput = styled.input`
   font-size: 20px;
   font-weight: bold;
   color: #212529;
-  padding: 15px 5px;
   border: 0px;
+  width: calc(100% - 5px);
+  background-color: #f3f4f6;
+  padding: 10px 5px;
+  border-radius: 5px;
+`;
+
+// Task > Duration
+
+const DurationWrapper = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const DurationInput = styled.input`
   font-weight: bold;
   font-size: 16px;
-  width: 2rem;
+  color: #212529;
+  width: 1.5rem;
   border: 0px;
+  border-radius: 5px;
   text-align: right;
-  margin-right: 5px;
+  padding-top: 4px;
+  padding-right: 5px;
+  margin-right: 3px;
+  background-color: #f3f4f6;
 `;
 
-const PickerWrapper = styled.div`
+// Task > Color
+
+const ColorWrapper = styled.div`
   display: flex;
-  justify-content: space-between;
   align-items: center;
 `;
 
@@ -168,26 +210,31 @@ const ColorOrb = styled.div`
   margin-right: 5px;
 `;
 
+// Buttons
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  margin: 15px 0px;
+`;
+
 const AddTaskButton = styled.input`
   background: #3b82f6;
   color: white;
   font-weight: bold;
-  color: white;
-  padding: 10px;
-  margin-top: 20px;
   border: 0px;
   border-radius: 5px;
+  flex-grow: 5;
+  margin-right: 5px;
+  padding: 10px 0px;
 `;
 
-const CancelButton = styled.input``;
-
-const DurationWrapper = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const ColorWrapper = styled.div`
-  display: flex;
-  margin-right: 16px;
-  align-items: center;
+const CancelButton = styled.input`
+  background: #4b5563;
+  color: white;
+  font-weight: bold;
+  border: 0px;
+  border-radius: 5px;
+  flex-grow: 1;
+  margin-left: 5px;
+  padding: 10px 0px;
 `;
